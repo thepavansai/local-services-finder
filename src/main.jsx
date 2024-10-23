0fb1c1a21901f6e69fb2b45906808ce63b716510
thepavansai <pavansaisriram@gmail.com>
2024-10-23 17:28:28 +0530

    add entry service with pincode and local db connection

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Add_Entry from './Add_Entry.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+   <Add_Entry/>
+  </StrictMode>
 )
